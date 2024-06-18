e4af7c771606c8050423ddfda3d1c4b3bf1921f9
BuildTools <unconfigured@null.spigotmc.org>
2024-06-19 01:43:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Test API",
+        Description = "API description",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test API V1");
+    });
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
